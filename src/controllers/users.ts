779fe6b699ca73ad71e197da0395168a1b5031db
Chinyere <157247073+cchukwuetoo@users.noreply.github.com>
2024-02-09 16:35:36 +0100

    Update users.ts fix

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -25,22 +25,43 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 };
 
 
+// export const updateUser = async (req: express.Request, res: express.Response) => {
+//     try {
+//         const { id } = req.params;
+//         const { username } = req.body;
+
+//         if (!username) {
+//             return res.sendStatus(400);
+//         }
+//         const user = await getUserById(id);
+
+//         user.username = username;
+//         await user.save();
+
+//         return res.sendStatus(200).json(user).end()
+//     } catch (error) {
+//         console.log(error);
+//         return res.sendStatus(400);
+//     }
+// }
+
+
 export const updateUser = async (req: express.Request, res: express.Response) => {
     try {
         const { id } = req.params;
         const { username } = req.body;
 
         if (!username) {
-            return res.sendStatus(400);
+            return res.status(400).send('Username is required');
         }
         const user = await getUserById(id);
 
         user.username = username;
         await user.save();
 
-        return res.sendStatus(200).json(user).end()
+        return res.status(200).json(user);
     } catch (error) {
         console.log(error);
-        return res.sendStatus(400);
+        return res.status(400).send('An error occurred');
     }
 }
